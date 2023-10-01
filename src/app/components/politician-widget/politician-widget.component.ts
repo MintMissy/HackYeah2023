111ad62edf69cb27a641134c9676c822c29267d8
Mint Missy <dawidkostka480@gmail.com>
2023-10-01 10:34:31 +0200

    politician widget

--- src/app/components/politician-widget/politician-widget.component.ts
+++ src/app/components/politician-widget/politician-widget.component.ts
@@ -1,6 +1,7 @@
 import {Component, HostBinding, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Router} from "@angular/router";
+import {IconComponent} from "../icon/icon.component";
 
 export interface PoliticianWidget {
   id: string;
@@ -12,13 +13,13 @@ export interface PoliticianWidget {
 @Component({
 	selector: 'app-politician-widget',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, IconComponent],
 	templateUrl: './politician-widget.component.html',
 	styleUrls: ['./politician-widget.component.scss'],
 })
 export class PoliticianWidgetComponent {
 	@Input() politicianData: PoliticianWidget = {
-    id: '1',
+		id: '1',
 		avatarSrc:
 			'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Krzysztof_Kononowicz.JPG/1200px-Krzysztof_Kononowicz.JPG',
 		firstName: 'Jacek',
@@ -27,9 +28,8 @@ export class PoliticianWidgetComponent {
 
 	@Input({ required: true }) state: 'follow' | 'unfollow' = 'unfollow';
 
-  constructor(private router: Router) {
-  }
-  navigateToPoliticianProfile() {
-    this.router.navigateByUrl(`/profile/politician/${this.politicianData.id}`)
-  }
+	constructor(private router: Router) {}
+	navigateToPoliticianProfile() {
+		this.router.navigateByUrl(`/profile/politician/${this.politicianData.id}`);
+	}
 }
