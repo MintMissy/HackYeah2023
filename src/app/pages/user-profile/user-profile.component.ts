5f8e31c1a9a9a6e4ae2594d8cb41bedf3c2b21bd
Mint Missy <dawidkostka480@gmail.com>
2023-10-01 11:16:42 +0200

    user profile page

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {PoliticianWidgetComponent} from "../../components/politician-widget/politician-widget.component";
+import {
+	PoliticianWidget,
+	PoliticianWidgetComponent,
+} from '../../components/politician-widget/politician-widget.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { PostComponent } from '../politician-profile/post/post.component';
+import { ProfileDetailsComponent } from '../politician-profile/profile-details/profile-details.component';
+import { VideoListComponent } from '../../components/video-list/video-list.component';
+import { VotingMarkersComponent } from '../../components/voting-markers/voting-markers.component';
+import { IconComponent } from '../../components/icon/icon.component';
 
 @Component({
 	selector: 'app-user-profile',
 	standalone: true,
-	imports: [CommonModule, PoliticianWidgetComponent],
+	imports: [
+		CommonModule,
+		PoliticianWidgetComponent,
+		MatIconModule,
+		MatTabsModule,
+		PostComponent,
+		ProfileDetailsComponent,
+		VideoListComponent,
+		VotingMarkersComponent,
+		IconComponent,
+	],
 	templateUrl: './user-profile.component.html',
 	styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {}
+export class UserProfileComponent {
+	politicianData: PoliticianWidget[] = [
+		{
+			id: '1',
+			avatarSrc:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Krzysztof_Kononowicz.JPG/1200px-Krzysztof_Kononowicz.JPG',
+			firstName: 'Jacek',
+			lastName: 'Pato',
+		},
+	];
+}
